ed7eb81c162a1da13447e9a106575f823d907b3f
Ubuntu <ubuntu@ip-172-31-19-174.us-east-2.compute.internal>
2020-03-17 23:13:48 +0000

    Add Sac County Daily

---
+++ ca_covid_county_spiders/spiders/sac_county_daily.py
@@ -0,0 +1,25 @@
+import scrapy
+from ca_covid_county_spiders.utils.markdown import markdownIt
+from ca_covid_county_spiders.utils.seasoning import salt, hasCovid
+
+class SacCountySpider(scrapy.Spider):
+    name = "sac_county_daily"
+    start_urls = [
+        'https://www.saccounty.net/news/latest-news/Pages/2020-Latest-News-Archive.aspx',
+    ]
+
+    def parse(self, response):
+        for link in response.css('li.dfwp-item h3 a'):
+            yield response.follow(link, self.parse_post)
+    
+    def parse_post(self, response):
+        page = response.css('div.article').get()
+        data = salt(response, {
+            'title': response.css('div.heading h2::text').get(),
+            'content': markdownIt(page),
+        })
+        if hasCovid(data):
+            yield data
+        else:
+            return
+        
--- ca_covid_county_spiders/utils/seasoning.py
+++ ca_covid_county_spiders/utils/seasoning.py
@@ -1,5 +1,34 @@
 from datetime import datetime
 
+def searchDict(myDict, term):
+    findings = []
+    for key, value in myDict.items():
+        if term in value:
+            findings.append(key)
+    return findings
+
+def hasCovid(payload):
+    keywords = [
+        'COVID-19',
+        'COVID19',
+        'COVID',
+        'coronavirus',
+        'corona virus',
+        'corona',
+        'SARS-CoV-2',
+        'SARSCov2',
+        'SARS CoV 2',
+    ]
+    findings = []
+    for keyword in keywords:
+        query = searchDict(payload, keyword)
+        if query:
+            findings = findings + query
+    if findings:
+        return True
+    else: 
+        return False
+                
 def mergeDicts(dict1, dict2): 
     dict1.update(dict2)
     return dict1
